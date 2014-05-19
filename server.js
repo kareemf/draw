9c18124807f2562cbe9ebb94fa377e72ebfb29f5
Kareem Francis <francis.kareem+git@gmail.com>
2014-05-18 20:52:28 -0400

    cleaned up server code

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-var express = require('express'),
-    bodyParser = require('body-parser');
+'use strict';
 
-var app = express();
+var express = require('express'),
+    app = express();
+
+app.use(express.static(__dirname + '/public'));
+
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
-var io = require('socket.io').listen(server);
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
+var io = require('socket.io').listen(server);
 
 app.get('/', function index(req, res){
   res.sendfile(__dirname + '/public/canvas.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  io.sockets.emit('user connected');
+
+  socket.on('message', function (from, data) {
+    console.log('message from', from, '. data', data);
+  });
+
+  socket.on('disconnect', function () {
+    io.sockets.emit('user disconnected');
+  });
 });
