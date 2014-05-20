354c4313480b9282f574c580575a6ff0cfa8f32c
Kareem Francis <francis.kareem+git@gmail.com>
2014-05-19 22:42:29 -0400

    added redis to package.json, redis require to server.js, removed 'from' arg from client and server emit, moved to message data object. when message is recieved by server, stored in redis. when client connnects to server, redis data is sent to client

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express'),
+    redis = require('redis'),
     app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -12,19 +13,44 @@ var server = app.listen(3000, function() {
 var io = require('socket.io').listen(server);
 
 app.get('/', function index(req, res){
-  res.sendfile(__dirname + '/public/canvas.html');
+    res.sendfile(__dirname + '/public/canvas.html');
+});
+
+var redisclient = redis.createClient();
+
+redisclient.on("error", function (err) {
+    console.log("redis error " + err);
 });
 
 io.sockets.on('connection', function (socket) {
-  io.sockets.emit('user connected');
+    socket.broadcast.emit('user connected');
 
-  socket.on('message', function (from, data) {
-    console.log('message from', from, '. data', data);
-    // sending to all clients except sender
-    socket.broadcast.emit('message', from, data);
-  });
+    //retrieve and send the history of previously recieved messages
+    redisclient.lrange('messages', 0, -1, function(err, items){
+        if(err){
+            console.log("error retrieving messages from redis: " + err);
+        }
+        else{
+            console.log(items.length + " items:");
+            items.forEach(function (reply, i) {
+                console.log("    " + i + ": " + reply);
+            });
 
-  socket.on('disconnect', function () {
-    io.sockets.emit('user disconnected');
-  });
+            socket.emit('messages', items);
+        }
+    });
+
+    socket.on('message', function (data) {
+        console.log('message from', data.from, '. data', data);
+
+        //add message to history
+        redisclient.rpush('messages', data);
+
+        // sending to all clients except sender
+        socket.broadcast.emit('message', data);
+    });
+
+    socket.on('disconnect', function () {
+        socket.broadcast.emit('user disconnected');
+    });
 });
